1fbd0e32453e006834fc4c790de3608cee20281c
Sudhansu <50366468+sudhansu-s-rath@users.noreply.github.com>
2025-07-05 19:21:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,15 @@ st.write("Click a point on the map to view rainfall time series at that location
 ds = load_data()
 
 # Calculate spatial mean for map
-avg_map = avg_map.rename({'lon': 'x', 'lat': 'y'})
-avg_map.rio.set_spatial_dims(x_dim="x", y_dim="y", inplace=True)
+avg_map = ds['pr'].mean(dim='time')  # Average over time
+
+# Set CRS and spatial dims explicitly
+avg_map.rio.set_spatial_dims(x_dim="lon", y_dim="lat", inplace=True)
 avg_map.rio.write_crs("EPSG:4326", inplace=True)
+
+# Export to GeoTIFF
 avg_map.rio.to_raster("temp.tif")
+
 
 m = leafmap.Map(center=[20.5, 85.5], zoom=5)
 m.add_raster("temp.tif", layer_name="Mean Extreme Precip")
